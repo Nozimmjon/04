9b0a1ea3071a143955a6dec8be425d5d5b2b96b5
Jaloliddin Usmonov <J.Usmonov@cerr.dc>
2023-02-08 20:21:13 +0500

    Farg'ona

--- script/fergana/fergana_tables_2023_01.R
+++ script/fergana/fergana_tables_2023_01.R
@@ -5,11 +5,12 @@ fergana_input_02 %>%
   tabyl(district, q_1) %>%
   adorn_percentages() %>% 
   select(district, "Ёмонлашади", "Ўзгармайди", "Яхшиланади") %>% 
-  mutate_at(vars(-district), as.double) %>% 
+  mutate_at(vars(-district), as.numeric) %>% 
   arrange(desc(across(starts_with("Ёмонлашади")))) %>% 
   gt(rowname_col = "district") %>% 
   tab_header(title = md("*1-савол.* **3 ойдан сўнг Ўзбекистонда <span style='color:red'>иқтисодий ҳолат</span> қандай ўзгаради?**"),
              subtitle = md("(*Респондентларнинг жавоблари*)")) %>% 
+  fmt_percent(columns = c(2:4), decimals = 0) %>% 
   my_theme_gt() %>% 
   cols_width(everything() ~ px(180)) %>% 
   gtsave('1_savol.png', path = here("results", "tables", "fergana"))
@@ -40,7 +41,7 @@ fergana_input_02 %>%
              subtitle = md("*(Респондентларнинг жавоблари)*")) %>% 
   my_theme_gt() %>% 
   cols_width(everything() ~ px(180)) %>% 
-  gtsave('5_savol.png', path = here("results", "tables", "fergana"))
+  gtsave('5_savol.png', path = here::here("results", "tables", "fergana"))
 
 #table 2
 fergana_input_02 %>%
@@ -85,84 +86,11 @@ fergana_input_02 %>%
   cols_width(everything() ~ px(180)) %>% 
   gtsave('6_savol.png', path = here("results", "tables", "fergana"))
 
-#table 7
 
-# table_7 <- fergana_input_02 %>% add_count(district) %>% 
-#   separate_rows(q_7, sep = ",") %>%
-#   mutate(q_7 = str_trim(q_7)) %>% 
-#   count(district, n,  q_7) %>% 
-#   mutate(freq = 100*nn/n) %>% 
-#   select(-n, -nn) %>% 
-#   pivot_wider(names_from = q_7, values_from = freq) 
-# 
-# write_xlsx(table_7, "muammolar_fergana.xlsx")
-
-# table 8 uy isitish manbalari
+#table 12 mahalliy organlarni baholash 
 
 fergana_input_02 %>%
-  mutate(q_8 = str_replace_all(q_8, "(электропечка, пушка, кондиционер ва х.к.)", "")) %>% 
-  mutate(q_8 = str_replace_all(q_8, "(тёплый пол, АГВ ёки котёл)", "")) %>% 
-  mutate(q_8 = str_replace_all(q_8, "Апилка билан", "опилка")) %>% 
-  mutate(q_8 = str_replace_all(q_8, "ОПИЛЬКА", "опилка")) %>% 
-  add_count(district) %>% 
-  separate_rows(q_8, sep = ",") %>%
-  mutate(q_8 = str_trim(q_8)) %>% 
-  mutate(q_8 = recode(q_8, 
-                      "Кўмир ва кўмир брикетлари" = "Кўмир ва кўмир брикетлари",
-                      "Суюлтирилган газ (баллон)" = "Суюлтирилган газ (баллон)",
-                      "Марказлашган иссиқлик тизими (Центральное отопление)" = "Марказлашган иссиқлик тизими",
-                      "Ўтин" = "Ўтин",
-                      "Электр таъминоти ()" = "Электр таъминоти",
-                      "Табиий газ таъминоти ( )" = "Табиий газ таъминоти",
-                      .default = "Бошқа")) %>% 
-  count(district, n, q_8) %>% 
-  mutate(freq = nn/n) %>% 
-  select(-n, -nn) %>% 
-  pivot_wider(names_from = q_8, values_from = freq, values_fill = 0) %>% 
-  mutate_at(vars(-district), as.double) %>%
-  #arrange(desc(across(starts_with("Жуда ёмон")))) %>%
-  gt(rowname_col = "district") %>%
-  tab_header(title = md("**Уйингизни иситиш учун асосан қандай манбалардан фойдаланасиз?**"),
-             subtitle = md("(*Респондентларнинг жавоблари)*")) %>%
-  cols_width(everything() ~ px(120)) %>%
-  my_theme_gt() %>%
-  gtsave('иситиш_манбалари.png', path = here("results", "tables", "fergana"), vwidth = 1500, vheight = 1000)
-
-#table 10 qishga tayyorlik
-
-fergana_input_02 %>%
-  tabyl(district, q_10) %>%
-  adorn_percentages() %>%
-  select(district, "Ҳа, тўлиқ тайёр",  
-         "Тайёр эмас",
-         "Қисман"
-  ) %>%
-  mutate_at(vars(-district), as.double) %>%
-  arrange(desc(across(starts_with("Ҳа, тўлиқ тайёр")))) %>%
-  gt(rowname_col = "district") %>%
-  tab_header(title = md("**Уй хўжалигингиз куз-қиш мавсумига тайёрми?**"),
-             subtitle = md("(*Респондентларнинг жавоблари)*")) %>%
-  cols_width(everything() ~ px(120)) %>%
-  my_theme_gt() %>%
-  gtsave('winter_readiness.png', path = here("results", "tables", "fergana"), vwidth = 1500, vheight = 1000)
-
-# isitish bilan bogliq muammolar 
-
-fergana_input_02 %>%
-  tabyl(district, q_11) %>%
-  adorn_percentages() %>%
-  mutate_at(vars(-district), as.double) %>%
-  gt(rowname_col = "district") %>%
-  tab_header(title = md("**Куз-қиш мавсумида иситиш билан боғлиқ энг катта муаммо**"),
-             subtitle = md("(*Респондентлар жавоблари)*")) %>%
-  cols_width(everything() ~ px(125)) %>%
-  my_theme_gt() %>%
-  gtsave('winter_readiness_2.png', path = here("results", "tables", "fergana"), vwidth = 1500, vheight = 1000)
-
-#table 11 mahalliy organlarni baholash 
-
-fergana_input_02 %>%
-  tabyl(district, q_12) %>%
+  tabyl(district, q_9) %>%
   adorn_percentages() %>% 
   select(district, "Жуда ёмон", "Ёмон", "Ўртача", "Яхши", "Жуда яхши") %>% 
   mutate_at(vars(-district), as.double) %>% 
@@ -174,67 +102,44 @@ fergana_input_02 %>%
   my_theme_gt() %>% 
   gtsave('mahalliy_organ.png', path = here("results", "tables", "fergana"))
 
+# table 8 uy isitish manbalari
 
-# #table 8 hokim yordamchisi
-# 
-# fergana_input_02 %>%
-#   tabyl(district, q_8) %>%
-#   adorn_percentages() %>%
-#   select(district, "Танимайман",  
-#          "Ҳеч қандай ёрдам бергани йўқ", 
-#          "Имтиёзли кредит олишда кўмаклашди", 
-#          "Ишга жойлаштиришга ёрдам берди",
-#          "Субсидия, грант ва моддий ёрдам тақдим этилди",
-#          "Малака оширишга кўмаклашди",
-#          "Ер ажратишда ёрдам берди"
-#   ) %>%
-#   mutate_at(vars(-district), as.double) %>%
-#   #arrange(desc(across(starts_with("Жуда ёмон")))) %>%
-#   gt(rowname_col = "district") %>%
-#   tab_header(title = md("**Ҳоким ёрдамчиси томонидан кўрсатилган ёрдам ҳолати**"),
-#              subtitle = md("(*Респондентларнинг жавоблари)*")) %>%
-#   cols_width(everything() ~ px(120)) %>%
-#   my_theme_gt() %>%
-#   gtsave('hokim_yordamchisi.png', path = here("results", "tables", "fergana"))
-# 
-# #table 9 yoshlar yetakchisi faoli
-# 
-# fergana_input_02 %>%
-#   filter(age <= "30") %>% 
-#   tabyl(district, q_9) %>%
-#   adorn_percentages() %>%
-#   select(district, "Танимайман",  
-#          "Ёрдам олганман",
-#          "Фаолиятидан хабардорман, лекин ёрдам олмаганман", 
-#          "Танийман, лекин ёрдамга зарурият йўқ") %>%
-#   mutate_at(vars(-district), as.double) %>%
-#   #arrange(desc(across(starts_with("Жуда ёмон")))) %>%
-#   gt(rowname_col = "district") %>%
-#   tab_header(title = md("**Ёшлар етакчиси фаолияти билан танишмисиз?**"),
-#              subtitle = md("(*Респондентларнинг жавоблари)*")) %>%
-#   cols_width(everything() ~ px(120)) %>%
-#   my_theme_gt() %>%
-#   gtsave('yoshlar_yetakchisi.png', path = here("results", "tables", "fergana"))
-# 
-# 
-# #table 10 ayollar faoli
-# 
-# fergana_input_02 %>%
-#   filter(gender == "Аёл") %>% 
-#   tabyl(district, q_10) %>%
-#   adorn_percentages() %>%
-#   select(district, "Танимайман",  
-#          "Ёрдам олганман",
-#          "Фаолиятидан хабардорман, лекин ёрдам олмаганман", 
-#          "Танийман, лекин ёрдамга зарурият йўқ") %>%
-#   mutate_at(vars(-district), as.double) %>%
-#   #arrange(desc(across(starts_with("Жуда ёмон")))) %>%
-#   gt(rowname_col = "district") %>%
-#   tab_header(title = md("**Хотин-қизлар фаоли билан танишмисиз?**"),
-#              subtitle = md("(*Аёл-қиз респондентларнинг жавоблари)*")) %>%
-#   cols_width(everything() ~ px(120)) %>%
-#   my_theme_gt() %>%
-#   gtsave('ayollar_yetakchisi.png', path = here("results", "tables", "fergana"))
+fergana_input_02 %>%
+  mutate(q_8 = str_replace_all(q_8, "(тез тиббий ёрдам, поликлиника, шифохоналар ва дорихоналар мавжудлиги)", "")) %>% 
+  add_count(district) %>% 
+  separate_rows(q_8, sep = ",") %>%
+  mutate(q_8 = str_trim(q_8)) %>% 
+  mutate(q_8 = recode(q_8, 
+                      "Соғлиқни сақлаш хизматлари сифатини ошириш борасидаги ишлар" = "Соғлиқни сақлаш сифатини ошириш",
+                      "Мактабгача таълим тизими билан қамров даражасини ошириш борасидаги ишлар" = "Мактабгача таълим тизими билан қамровни ошириш",
+                      "Мактабда таълим сифатини ошириш борасидаги ишлар" = "Мактабда таълим сифатини ошириш",
+                      "Аҳолини уй-жой билан таъминлаш ишларини" = "Аҳолини уй-жой билан таъминлаш",
+                      "Маҳаллабай ишлаш ва ҳоким ёрдамчилари тизимидаги ишларни" = "Маҳаллабай ишлаш ва ҳоким ёрдамчилари ",
+                      "Ўқитувчиларни қўллаб-қувватлаш ишларини" = "Ўқитувчиларни қўллаб-қувватлаш",
+                      "Шифокорларни қўллаб-қувватлаш ишларини" = "Шифокорларни қўллаб-қувватлаш",
+                      "Коррупцияга қарши курашишлар" = "Коррупцияга қарши курашиш",
+                      "Камбағаликка қарши кураш ишларини" = "Камбағаликка қарши кураш",
+                      "Олий таълим тизимидаги ислоҳотларни" = "Олий таълим тизимидаги ислоҳотларни",
+                      "Тадбиркорларни қўллаб-қувватлаш ишларини" = "Тадбиркорларни қўллаб-қувватлаш",
+                      "Автомобил сотиб олиш жараёнларини" = "Автомобил сотиб олиш жараёнларини",
+                      "Газ таъминоти соҳаси" = "Газ таъминоти",
+                      "Иш билан таъминлаш соҳаси" = "Бандликни таъминлаш",
+                      "Электр таъминоти соҳаси" = "Электр таъминоти",
+                      .default = "Бошқа")) %>% 
+  count(district, n, q_8) %>% 
+  mutate(freq = nn/n) %>% 
+  select(-n, -nn) %>% 
+  pivot_wider(names_from = q_8, values_from = freq, values_fill = 0) %>% 
+  mutate_at(vars(-district), as.double) %>%
+  #arrange(desc(across(starts_with("Жуда ёмон")))) %>%
+  gt(rowname_col = "district") %>%
+  tab_header(title = md("**8.	Қайси соҳадаги ислоҳотларни тезлаштириш керак деб ўйлайсиз?**"),
+             subtitle = md("(*Респондентларнинг жавоблари)*")) %>%
+  cols_width(everything() ~ px(120)) %>%
+  my_theme_gt() %>%
+  gtsave('islohotlar_tezlash.png', path = here("results", "tables", "fergana"), vwidth = 1500, vheight = 1000)
+
+#table 10 qishga tayyorlik
 
 
 #table ishsizlik
@@ -294,4 +199,3 @@ fergana_input_02 %>% filter(income != "Жавоб беришдан бош тор
   cols_width(everything() ~ px(150)) %>% 
   my_theme_gt() %>% 
   gtsave('income.png', path = here("results", "tables", "fergana"))    
-
